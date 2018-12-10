265e722cff8f742ef2d7b7da21c0b77cb93d1f5f
Rathinasamy, Pradeep <pradeep.rathinasamy@wellpoint.com>
2018-12-10 12:34:09 +0530

    fixed issue

--- task-manager-service/src/main/java/com/task/manager/Application.java
+++ task-manager-service/src/main/java/com/task/manager/Application.java
@@ -1,10 +1,12 @@
-package com.task.manager.app;
+package com.task.manager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +14,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import com.task.manager.app.security.CORSFilter;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.task.*")
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
